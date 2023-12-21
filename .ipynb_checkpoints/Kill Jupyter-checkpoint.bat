5c90a9852baefe5d536a8e104c631bf2dd514260
bao kuanqing <F7691199@idsbg.lh.com>
2023-12-21 15:14:18 +0800

    init

---
+++ .ipynb_checkpoints/Kill Jupyter-checkpoint.bat
@@ -0,0 +1,6 @@
+%隐藏cmd窗口%
+@echo off
+if "%1"=="h" goto begin
+start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+taskkill /f /im jupyter-notebook.exe
